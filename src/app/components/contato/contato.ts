95545a2513f89e2e5e9a1ec764571ab2a84f03f2
Matheus Lira <mathgomes16@gmail.com>
2026-05-16 09:35:34 -0300

    ajustes design

--- src/app/components/contato/contato.ts
+++ src/app/components/contato/contato.ts
@@ -9,11 +9,11 @@ import { CommonModule } from '@angular/common';
 })
 export class ContatoComponent {
   contacts = [
-      { icon: '✉', label: 'matheusgslira@outlook.com', href: 'mailto:matheusgslira@outlook.com' },
+      { icon: '✉', label: 'Outlook', href: 'mailto:matheusgslira@outlook.com' },
       { icon: '📞', label: '(87) 98114-6123', href: 'tel:+5587981146123' },
       { icon: '📍', label: 'Recife - PE', href: '#' },
-      { icon: '💼', label: 'linkedin.com/in/matheus-lira-a18006158', href: 'https://www.linkedin.com/in/matheus-lira-a18006158/' },
-      { icon: '🐙', label: 'github.com/matheusglira', href: 'https://github.com/matheusglira' },
-      { icon: '📷', label: 'instagram.com/matheusgslira', href: 'https://www.instagram.com/matheusgslira/' },
+      { icon: '💼', label: 'LinkedIn', href: 'https://www.linkedin.com/in/matheus-lira-a18006158/' },
+      { icon: '🐙', label: 'GitHub', href: 'https://github.com/matheusglira' },
+      { icon: '📷', label: 'Instagram', href: 'https://www.instagram.com/matheusgslira/' },
   ];
 }
